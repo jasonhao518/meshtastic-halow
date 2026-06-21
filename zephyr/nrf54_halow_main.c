6c6c38a4e2e93e6cfe312af7e8c9ff7b52270b42
Jason Hao <jasonhao518@gmail.com>
2026-06-21 02:22:49 +0200

    add logging

--- zephyr/nrf54_halow_main.c
+++ zephyr/nrf54_halow_main.c
@@ -2,11 +2,12 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
-LOG_MODULE_REGISTER(nrf54_halow, LOG_LEVEL_INF);
+LOG_MODULE_REGISTER(nrf54_halow, LOG_LEVEL_DBG);
 
 int main(void)
 {
     LOG_INF("Meshtastic nRF54L15 HaLow bring-up starting");
+    LOG_DBG("nRF54L15 HaLow debug logging enabled on SEGGER RTT/J-Link");
 
 #if DT_NODE_EXISTS(DT_NODELABEL(morse_wifi))
     const struct device *morse = DEVICE_DT_GET(DT_NODELABEL(morse_wifi));
